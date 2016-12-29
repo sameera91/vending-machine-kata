26ab73e24a22edc1e8d4f75ad01fe4d0d3fb55d2
sameera91 <sameera.bachoti@gmail.com>
2016-12-29 17:18:22 -0500

    rspec test for select product

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -13,6 +13,23 @@ describe './lib/vending-machine.rb' do
         expect($stdout).to receive(:puts).with("bye.")
         vendingmachine.insert_coin
       end
+
+      it 'allows customers to select products' do
+        vendingmachine = VendingMachine.new
+        expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+        expect(vendingmachine).to receive(:gets).and_return("quarter")
+        expect($stdout).to receive(:puts).with(0.25)
+        expect(vendingmachine).to receive(:gets).and_return("quarter")
+        expect($stdout).to receive(:puts).with(0.50)
+        expect(vendingmachine).to receive(:gets).and_return("select product")
+        expect($stdout).to receive(:puts).with("Select a product. Valid options include: cola, chips or candy.")
+        expect(vendingmachine).to receive(:gets).and_return("chips")
+        expect($stdout).to receive(:puts).with("THANK YOU. Your change is: 0.0")
+        expect(vendingmachine).to receive(:gets).and_return("exit")
+        expect($stdout).to receive(:puts).and_return("Invalid product.").at_least(:twice)
+        expect(vendingmachine).to receive(:gets).and_return("exit")
+        vendingmachine.insert_coin
+      end
     end
   end
 end
