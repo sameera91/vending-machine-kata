c25c3c1b5d3068b9908a7841ec925174c8699c57
sameera91 <sameera.bachoti@gmail.com>
2016-12-30 20:01:39 -0500

    changed output messages

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -5,7 +5,7 @@ describe VendingMachine do
   describe 'lib/vending-machine.rb' do
     it 'accepts coins so that I can collect money from the customer' do
       vendingmachine = VendingMachine.new
-      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like to your coins to be returned. Enter 'exit' if you would like to exit.").at_least(:twice)
       expect(vendingmachine).to receive(:gets).and_return("quarter")
       expect($stdout).to receive(:puts).with(0.25)
       expect(vendingmachine).to receive(:gets).and_return("exit")
@@ -15,9 +15,9 @@ describe VendingMachine do
 
     it 'rejects invalid coins' do
       vendingmachine = VendingMachine.new
-      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like to your coins to be returned. Enter 'exit' if you would like to exit.").at_least(:twice)
       expect(vendingmachine).to receive(:gets).and_return("penny")
-      expect($stdout).to receive(:puts).with("invalid coin.")
+      expect($stdout).to receive(:puts).with("Invalid coin.")
       expect(vendingmachine).to receive(:gets).and_return("exit")
       expect($stdout).to receive(:puts).with("bye.")
       vendingmachine.insert_coin
@@ -25,13 +25,13 @@ describe VendingMachine do
 
     it 'allows customers to select products' do
       vendingmachine = VendingMachine.new
-      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like to your coins to be returned. Enter 'exit' if you would like to exit.").at_least(:twice)
       expect(vendingmachine).to receive(:gets).and_return("quarter")
       expect($stdout).to receive(:puts).with(0.25)
       expect(vendingmachine).to receive(:gets).and_return("quarter")
       expect($stdout).to receive(:puts).with(0.50)
       expect(vendingmachine).to receive(:gets).and_return("select product")
-      expect($stdout).to receive(:puts).with("Select a product. Valid options include: cola, chips or candy.")
+      expect($stdout).to receive(:puts).with("SELECT PRODUCT. Valid options include: cola, chips or candy. Enter exit if you would like to go back.")
       expect(vendingmachine).to receive(:gets).and_return("chips")
       expect($stdout).to receive(:puts).with("THANK YOU. Your change is: 0.0")
       expect(vendingmachine).to receive(:gets).and_return("exit")
@@ -42,7 +42,7 @@ describe VendingMachine do
 
     it 'allows customers to have their money returned' do
       vendingmachine = VendingMachine.new
-      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like to your coins to be returned. Enter 'exit' if you would like to exit.").at_least(:twice)
       expect(vendingmachine).to receive(:gets).and_return("quarter")
       expect($stdout).to receive(:puts).with(0.25)
       expect(vendingmachine).to receive(:gets).and_return("quarter")
@@ -54,12 +54,12 @@ describe VendingMachine do
       vendingmachine.insert_coin
     end
 
-    it 'tells customers when selected items is not available' do
+    it 'tells customers when selected item is not available' do
       vendingmachine = VendingMachine.new
       vendingmachine.instance_variable_set(:@cola_quantity, 0)
-      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.").at_least(:twice)
+      expect($stdout).to receive(:puts).with("INSERT COIN. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like to your coins to be returned. Enter 'exit' if you would like to exit.").at_least(:twice)
       expect(vendingmachine).to receive(:gets).and_return("select product")
-      expect($stdout).to receive(:puts).with("Select a product. Valid options include: cola, chips or candy.")
+      expect($stdout).to receive(:puts).with("SELECT PRODUCT. Valid options include: cola, chips or candy. Enter exit if you would like to go back.")
       expect(vendingmachine).to receive(:gets).and_return("cola")
       expect($stdout).to receive(:puts).with("SOLD OUT. Please select another item.")
       expect(vendingmachine).to receive(:gets).and_return("exit")
@@ -71,7 +71,7 @@ describe VendingMachine do
     it 'tells customers when exact change is required' do
       vendingmachine = VendingMachine.new
       vendingmachine.instance_variable_set(:@initial_change_value, 0)
-      expect($stdout).to receive(:puts).with("EXACT CHANGE ONLY. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you want to your coins to be returned.")
+      expect($stdout).to receive(:puts).with("EXACT CHANGE ONLY. Valid options include: nickel, dime, quarter. Enter 'select product' once you are ready to select a product. Enter 'return coins' if you would like your coins to be returned. Enter 'exit' if you would like to exit.")
       expect(vendingmachine).to receive(:gets).and_return("exit")
       expect($stdout).to receive(:puts).with("bye.")
       vendingmachine.insert_coin
